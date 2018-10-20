cfa3acb5b22681339c984d5e17189b817d671f0a
seankwalker <seankentonwalker@gmail.com>
2018-10-20 13:16:52 -0400

    working calendar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import {Heading, Pane} from "evergreen-ui";
 
 import { CollegeSelector } from "./components/CollegeSelector";
+import { Calendar } from "./components/Calendar";
 
 import styles from "./lib/styles.css"
 
@@ -14,6 +15,9 @@ class App extends Component {
             Yale Residential College Calendar
           </Heading>
           <CollegeSelector className={styles.headerContent} />
+        </Pane>
+        <Pane id={styles.calendar}>
+          <Calendar />
         </Pane>
       </div>
     );
