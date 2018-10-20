36e85660c8d2bb6aba079f59b729fbcec2a703e5
vagrant <vagrant@p4>
2018-10-20 17:12:18 +0000

    localizer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react'
 import { render } from 'react-dom'
-import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import { getEvents } from './gcal'
 
-import BigCalendar from 'react-big-calendar'
-// a localizer for BigCalendar
-BigCalendar.momentLocalizer(moment)
-
-// this weird syntax is just a shorthand way of specifying loaders
-require('style-loader!css-loader!react-big-calendar/lib/css/react-big-calendar.css')
+const localizer = BigCalendar.momentLocalizer(moment);
 
 class App extends React.Component {
   constructor () {
     super()
+    this.state = {
+      events: []
+    }
+  }
+  componentDidMount () {
+    getEvents((events) => {
+      this.setState({events})
+    })
   }
   render () {
     return (
       // React Components in JSX look like HTML tags
       <BigCalendar
         style={{height: '420px'}}
-        events={[]}
+        events={this.state.events}
+        localizer={localizer}
       />
     )
   }
