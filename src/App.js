e3b0f0ad46560edb09447cbd253bc9cb622ce99f
Alexander Briasco-Stewart <astewart.is1@gmail.com>
2018-10-20 13:00:23 -0400

    I have no idea what I'm doing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 
 import {Heading, Pane} from 'evergreen-ui';
+import moment from 'moment'
+
+import BigCalendar from 'react-big-calendar'
+import { getEvents } from './gcal'
+
+BigCalendar.momentLocalizer(moment)
+
+//require('style-loader!css-loader!react-big-calendar/lib/css/react-big-calendar.css')
+require('react-big-calendar/lib/css/react-big-calendar.css')
+
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      events: []
+    }
+  }
+
+  componentDidMount(){
+    getEvents((events)=>{
+      this.setState({events})
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        {/* TODO */}
+        <BigCalendar
+
+        startAccessor='startDate'
+        endAccessor='endDate'
+        date={moment().toDate()}
+        style={{height: '420px'}}
+        events={this.state.events}
+        
+
+        />
       </div>
     );
   }
--- src/calapi/cal.js
+++ src/calapi/cal.js
@@ -7,3 +7,9 @@ function getCalendarByID(id){
 function parseCalendar(){
 
 }
+
+
+function test(){
+  var calendarID = "yale.edu_0m2hiitilfrba1duifi9maf960@group.calendar.google.com"
+  var APIKey = "AIzaSyC82TYeG6yXixD1q4i_RTqDQC_JlfWJA8Q"
+}
